b8be31c7df58c1ee210321fcd3a2a554e96ab631
Álvaro Pérez Urbano <alvaro_beta@hotmail.com>
2022-02-20 12:44:54 +0100

    Ahora se pueden añadir libros desde el cliente

--- src/main/java/com/iesfranciscodelosrios/model/dao/BookDAO.java
+++ src/main/java/com/iesfranciscodelosrios/model/dao/BookDAO.java
@@ -1,6 +1,10 @@
 package com.iesfranciscodelosrios.model.dao;
 
 import com.iesfranciscodelosrios.model.Book;
+import com.iesfranciscodelosrios.model.Client;
+import com.iesfranciscodelosrios.model.Manager;
+import com.iesfranciscodelosrios.model.User;
+import com.iesfranciscodelosrios.utils.Operations;
 import com.iesfranciscodelosrios.utils.PersistenceUnit;
 import com.iesfranciscodelosrios.utils.Tools;
 
@@ -9,24 +13,42 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @MappedSuperclass
 public class BookDAO {
 
-    public static synchronized List<Book> getAllOnStockBooks(){
+    public static synchronized List<Book> getAllOnStockBooks() {
         List<Book> result = new ArrayList<>();
         EntityManager em = PersistenceUnit.createEM();
         em.getTransaction().begin();
-        try{
+        try {
             TypedQuery<Book> q = em.createNamedQuery("getStockedBooks", Book.class);
             result = new ArrayList<>(q.getResultList());
             em.getTransaction().commit();
-        }catch (NoResultException e){
-            System.out.println(Tools.ANSI_RED+"No hay libros disponibles para su venta en la base de datos, están todos agotados"+Tools.ANSI_RESET);
+        } catch (NoResultException e) {
+            System.out.println(Tools.ANSI_RED + "No hay libros disponibles para su venta en la base de datos, están todos agotados" + Tools.ANSI_RESET);
         } finally {
             PersistenceUnit.closeEM();
         }
+        return result;
+    }
+
+    public static synchronized LinkedHashMap<Operations.ServerActions, Object> registerBook(Book b) {
+        LinkedHashMap<Operations.ServerActions, Object> result = new LinkedHashMap<>();
+        EntityManager em = PersistenceUnit.createEM();
+        em.getTransaction().begin();
+        Book book = em.merge(b);
+        em.persist(book);
+        if (book.getId() == -1) {
+            em.flush();
+            book.setId(book.getId());
+        }
+        result.put(Operations.ServerActions.OperationOk, book);
+        System.out.println("El libro " + book.getTitle() + " se ha registrado correctamente");
+        em.getTransaction().commit();
+        PersistenceUnit.closeEM();
         return result;
     }
 }
--- src/main/java/com/iesfranciscodelosrios/utils/Operations.java
+++ src/main/java/com/iesfranciscodelosrios/utils/Operations.java
@@ -8,7 +8,7 @@ public class Operations implements Serializable {
     private static final long serialVersionUID = 1L;
     public enum UserOptions implements Serializable{
         Register(0),//ok
-        Login(1), //Menu
+        Login(1), //Menu si ok
         //opciones de cliente normal
         ViewOnStockBooks(2),//Menu
         ViewAccount(3),//Menu
@@ -16,10 +16,16 @@ public class Operations implements Serializable {
         ViewPurchaseHistory(5),//Menu->historial compra
         BuyItem(6),//OK
         //Ociones de un Operador
-        AddBook(7),
-        ChangeStock(8),
-        DeleteBook(9),
-        SeeClients(10);
+        AddBook(7), //menu añadir
+        AddBookAction(8),//enviar libro
+        ChangeStock(9), //menu libros para cambiar stock
+        ChangeStockAction(10),//enviar nuevos stocks
+        DeleteBook(10),//menu para eliminar libros
+        DeleteBookAction(11),//enviar borrar libro
+        SeeClients(12),//menu para ver clientes
+        SeeClientRemove(13),//opcion eliminar clientes
+        SeeClientsAdd(14),//opcion añadir cliente
+        SeeClientsModify(14);//opcion cambiar cliente
         @Serial
         private static final long serialVersionUID = 1L;
         private final int i;
